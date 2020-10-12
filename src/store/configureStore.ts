ea50970547eb0acf3cc41a884673df07abf506e7
Evan Tseng <evan.tseng@access-company.com>
2020-10-12 10:25:17 +0800

    Add react log

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware, Middleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import rootReducer, { initStoreState } from '../reducer/rootReducer'
 import { createEpicMiddleware } from 'redux-observable'
 import rootEpics from '../epics/rootEpic'
 
 const bindMiddleware = (middleware: Middleware[]) => {
+  if (process.env.NODE_ENV === 'development') {
+    const logger: any = createLogger({ diff: true, collapsed: true })
+    return applyMiddleware(...middleware, logger)
+  }
   return applyMiddleware(...middleware)
 }
 
