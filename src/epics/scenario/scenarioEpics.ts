bae46d27cd63c0ff5cda2afc3835f18e80279566
Evan Tseng <evan.tseng@access-company.com>
2020-10-18 18:12:01 +0800

    use ajax to call api

--- src/epics/scenario/scenarioEpics.ts
+++ src/epics/scenario/scenarioEpics.ts
@@ -1,49 +1,38 @@
 import { AnyAction } from 'redux'
 import { ActionsObservable, ofType } from 'redux-observable'
-import { scenarioActionTypes, getScenarioListActionSuccess, ScenarioList } from '../../reducer/scenario/scenarioActions'
+import {
+  scenarioActionTypes,
+  getScenarioListSuccessAction,
+  getScenarioListFailedAction,
+  deleteScenariosSuccessAction,
+  deleteScenariosFailedAction,
+} from '../../reducer/scenario/scenarioActions'
 import { of } from 'rxjs'
-import { catchError, exhaustMap, map } from 'rxjs/operators'
-import FakeScenarioImg from '../../assets/icons/fake-scenario.png'
+import { catchError, exhaustMap, map, switchMap } from 'rxjs/operators'
+import { ajax, AjaxResponse } from 'rxjs/ajax'
 
 
 export const getScenarioEpic = (action$: ActionsObservable<AnyAction>) =>
   action$.pipe(
     ofType(scenarioActionTypes.GET_SCENARIO_LIST),
     exhaustMap(() =>
-      of([
-        {
-          id: 1,
-          display_name: 'scenario1',
-          created_at: '2019-12-19T11:22:33Z',
-          updated_at: '2019-12-19T11:22:33Z',
-          thumbnail_url: FakeScenarioImg,
-        },
-        {
-          id: 2,
-          display_name: 'scenario2',
-          created_at: '2019-12-19T11:22:33Z',
-          updated_at: '2019-12-19T11:22:33Z',
-          thumbnail_url: FakeScenarioImg,
-        },
-        {
-          id: 3,
-          display_name: 'scenario3',
-          created_at: '2019-12-19T11:22:33Z',
-          updated_at: '2019-12-19T11:22:33Z',
-          thumbnail_url: FakeScenarioImg,
-        },
-        {
-          id: 4,
-          display_name: 'scenario4',
-          created_at: '2019-12-19T11:22:33Z',
-          updated_at: '2019-12-19T11:22:33Z',
-          thumbnail_url: FakeScenarioImg,
-        }
-      ]).pipe(
-        map((res: ScenarioList) => (getScenarioListActionSuccess(res))),
-        catchError(err => of({type: 'err', err}))
+      ajax.get('http://localhost:8080/v1/scenarios').pipe(
+        map((res: AjaxResponse) => (getScenarioListSuccessAction(res.response))),
+        catchError(err => of(getScenarioListFailedAction(err)))
       ),
     ),
   )
 
-export default [getScenarioEpic]
+export const deleteScenarioEpic = (action$: ActionsObservable<AnyAction>) =>
+  action$.pipe(
+    ofType(scenarioActionTypes.DELETE_SCENARIO),
+    switchMap((action) =>
+      ajax.delete(`http://localhost:8080/v1/scenarios/${action.payload}`).pipe(
+        map((res: AjaxResponse) => (deleteScenariosSuccessAction(res.response))),
+        catchError(err => of(deleteScenariosFailedAction(err)))
+      )
+    ),
+  )
+
+
+export default [getScenarioEpic, deleteScenarioEpic]
--- src/reducer/scenario/scenarioActions.ts
+++ src/reducer/scenario/scenarioActions.ts
@@ -3,13 +3,40 @@ import { Scenario } from '../../model/scenario'
 export const scenarioActionTypes = {
   GET_SCENARIO_LIST: '@scenario/GET_SCENARIO_LIST',
   GET_SCENARIO_LIST_SUCCESS: '@scenario/GET_SCENARIO_LIST_SUCCESS',
+  GET_SCENARIO_LIST_FAILED: '@scenarios/GET_SCENARIO_LIST_FAILED',
+
+  DELETE_SCENARIO: '@scenario/DELETE_SCENARIO',
+  DELETE_SCENARIO_SUCCESS: '@scenario/DELETE_SCENARIO_SUCCESS',
+  DELETE_SCENARIO_FAILED: '@scenarios/DELETE_SCENARIO_FAILED',
 }
 
 export const getScenarioListAction = () => ({
   type: scenarioActionTypes.GET_SCENARIO_LIST,
 })
+
 export type ScenarioList = Scenario[]
-export const getScenarioListActionSuccess = (payload: Scenario[]) => ({
+export const getScenarioListSuccessAction = (payload: Scenario[]) => ({
   type: scenarioActionTypes.GET_SCENARIO_LIST_SUCCESS,
   payload,
+})
+
+export const getScenarioListFailedAction = (error: any) => ({
+  type: scenarioActionTypes.GET_SCENARIO_LIST_FAILED,
+  error,
+})
+
+export type DeleteScenarioData = number[]
+export const deleteScenariosAction = (payload: DeleteScenarioData) => ({
+  type: scenarioActionTypes.DELETE_SCENARIO,
+  payload,
+})
+
+export const deleteScenariosSuccessAction = (payload: DeleteScenarioData) => ({
+  type: scenarioActionTypes.DELETE_SCENARIO_SUCCESS,
+  payload,
+})
+
+export const deleteScenariosFailedAction = (error: any) => ({
+  type: scenarioActionTypes.DELETE_SCENARIO_SUCCESS,
+  error,
 })
